3b05f686fcb7a18aa273f384db0a3b361cddcc66
Bradley Davy <bdavy@ocf.co.uk>
2024-12-20 10:23:55 +0000

    Keep adding to the introduction to python code base

---
+++ Introduction_to_Python/Conditional_Statements/main.py
@@ -0,0 +1,9 @@
+new_list = []
+new_item = 1
+
+if type(new_item) == int:
+    new_list.append(new_item) 
+
+
+
+    
---
+++ Introduction_to_Python/Variables/main.py
@@ -0,0 +1,25 @@
+exponent = 3
+raw_data = []
+processed_data = []
+
+print(f'Exponent is a int type: {type(exponent) == int}')
+print(f'Raw data is a list type: {type(raw_data) == list}')
+print(f'Processed data is a list type: {type(processed_data) == list}')
+
+if exponent == 3:
+    print('The exponent is 3!')
+
+elif exponent == 4:
+    print('The exponent is 4!')
+
+if len(raw_data) == 0:
+    print('The raw data is empty!')
+
+else:
+    print('The raw data is not empty!')
+
+if len(processed_data) == 0:
+    print('The processed data is empty!')
+
+print(f'Exponent is a int type: {type(exponent) == int}')
+print(f'Raw data is a list type: {type(raw_data) == list}')
